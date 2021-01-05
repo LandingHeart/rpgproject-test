3f89aa69bb1350e80867273e72ac86aa001e3ce4
Shinan Liu <Shinan.liu@yahoo.com>
2021-01-04 19:33:08 -0500

    minor change

---
+++ Assets/AnimationScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator _anim;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+}
